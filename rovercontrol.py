4d6351054267f36373b3aad8bbfd31439637c17f
clear2030 <nathanlien88@gmail.com>
2025-05-22 09:59:55 -0700

    Create rovercontrol.py

---
+++ rovercontrol.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+from gpiozero import Servo
+import os
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+host_name = '10.178.202.171'  # IP Address of Raspberry Pi
+host_port = 5000
+
+leftMotor = Servo(3)
+rightMotor = Servo(27)
+
+def getTemperature():
+    temp = os.popen("vcgencmd measure_temp").read()
+    return temp
+
+
+class MyServer(BaseHTTPRequestHandler):
+
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def _redirect(self, path):
+        self.send_response(303)
+        self.send_header('Content-type', 'text/html')
+        self.send_header('Location', path)
+        self.end_headers()
+
+    def do_GET(self):
+        html = '''
+           <html>
+           <body style="width:960px; margin: 20px auto;">
+           <h1 style="text-align:center;">Rover Control</h1>
+           <p style="color:white;">Current GPU temperature is {}</p>
+           <form action="/" method="POST">
+               <input type="submit" name="Stop" value="Stop" style="height:300px; width:300px; margin:0 auto; display:flex;">
+               <br>
+               <input type="submit" name="Forward" value="Forward" style="height:300px; width:300px; margin:0 auto; display:flex;">
+               <br>
+               <input type="submit" name="Left" value="Left" style="height:300px; width:300px; margin:0 auto; display:flex;">
+               <input type="submit" name="Right" value="Right" style="height:300px; width:300px; margin:0 auto; display:flex;">
+               <br>
+               <input type="submit" name="Backward" value="Backward" style="height:300px; width:300px; margin:0 auto; display:flex;">
+           
+           <p>Clock the text below!</p>
+           <p id="myP" onmousedown="mouseDown()" onmouseup="mouseUp()">
+           The mouseDown() function sets the color of this text to red.
+           The mouseUp() function sets the color of this text to blue.
+           </p>
+
+           <script>
+           function mouseDown() {
+               document.getElementById("myP").style.color = "red";
+           }
+
+           function mouseUp() {
+               document.getElementById("myP").style.color = "blue";
+           }
+           </script>
+           
+           </body>
+           </html>
+        '''
+        temp = getTemperature()
+        self.do_HEAD()
+        self.wfile.write(html.format(temp[5:]).encode("utf-8"))
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length).decode("utf-8")
+        post_data = post_data.split("=")[1]
+        
+        if post_data == 'Forward':
+            leftMotor.value = 0.8
+            rightMotor.value = 0.8
+        elif post_data == 'Backward':
+            leftMotor.value = -0.8
+            rightMotor.value = -0.8
+        elif post_data == 'Stop':
+            leftMotor.value = 0
+            rightMotor.value = 0
+
+        print("Rover is {}".format(post_data))
+        self._redirect('/')  # Redirect back to the root url
+
+
+# # # # # Main # # # # #
+
+if __name__ == '__main__':
+    http_server = HTTPServer((host_name, host_port), MyServer)
+    print("Server Starts - %s:%s" % (host_name, host_port))
+
+    try:
+        http_server.serve_forever()
+    except KeyboardInterrupt:
+        http_server.server_close()
