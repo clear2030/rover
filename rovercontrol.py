5ea63ea467efec226e99aafcee780e0d7e2202e5
clear2030 <nathanlien88@gmail.com>
2025-05-29 09:00:59 -0700

    Update rovercontrol.py

--- rovercontrol.py
+++ rovercontrol.py
@@ -5,7 +5,7 @@ import os
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 host_name = '10.178.202.171'  # IP Address of Raspberry Pi
-host_port = 5000
+host_port = 7000
 
 leftMotor = Servo(3)
 rightMotor = Servo(27)
@@ -31,34 +31,29 @@ class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
         html = '''
            <html>
+           <style>
+           </style>
            <body style="width:960px; margin: 20px auto;">
            <h1 style="text-align:center;">Rover Control</h1>
            <p style="color:white;">Current GPU temperature is {}</p>
            <form action="/" method="POST">
-               <input type="submit" name="Stop" value="Stop" style="height:300px; width:300px; margin:0 auto; display:flex;">
+               
+               <input type="submit" name="Stop" value="Stop" style="height:150px; width:150px; color:red;">
                <br>
                <input type="submit" name="Forward" value="Forward" style="height:300px; width:300px; margin:0 auto; display:flex;">
                <br>
-               <input type="submit" name="Left" value="Left" style="height:300px; width:300px; margin:0 auto; display:flex;">
-               <input type="submit" name="Right" value="Right" style="height:300px; width:300px; margin:0 auto; display:flex;">
+               <input type="submit" name="Left" value="Left" style="height:300px; width:300px; margin:0 auto; display:inline;">
+               <input type="submit" name="Right" value="Right" style="height:300px; width:300px; margin:0 auto; display:inline;">
                <br>
                <input type="submit" name="Backward" value="Backward" style="height:300px; width:300px; margin:0 auto; display:flex;">
+
+           <button data-test="55" onclick="testFunction(this);">button1</button>
            
-           <p>Clock the text below!</p>
-           <p id="myP" onmousedown="mouseDown()" onmouseup="mouseUp()">
-           The mouseDown() function sets the color of this text to red.
-           The mouseUp() function sets the color of this text to blue.
-           </p>
-
            <script>
-           function mouseDown() {
-               document.getElementById("myP").style.color = "red";
-           }
-
-           function mouseUp() {
-               document.getElementById("myP").style.color = "blue";
-           }
+              alert("scripts work");
            </script>
+           
+           
            
            </body>
            </html>
@@ -81,6 +76,12 @@ class MyServer(BaseHTTPRequestHandler):
         elif post_data == 'Stop':
             leftMotor.value = 0
             rightMotor.value = 0
+        elif post_data == 'Left':
+            leftMotor.value = -0.8
+            rightMotor.value = 0.8
+        elif post_data == 'Right':
+            leftMotor.value = 0.8
+            rightMotor.value = -0.8
 
         print("Rover is {}".format(post_data))
         self._redirect('/')  # Redirect back to the root url
